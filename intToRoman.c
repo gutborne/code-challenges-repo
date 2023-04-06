c4abb578f8713e11bfcb44f707783a71e3db27dc
Mateus Gutemberg <mateus.gut432@gmail.com>
2023-04-06 09:32:33 -0300

    new changes in the inToRoman.c added(maybe the last ones)

--- intToRoman.c
+++ intToRoman.c
@@ -14,11 +14,15 @@
 #include <math.h>
 char romanNumbersArray[16];
 
-//Create a function that returns the number of digits
-//create a fuction that applies the formation rule of decimal numbers
 
 /**
- * @brief This initial description will walk through step by step to get this solution 
+ * @brief This initial description will walk through step by step to get this solution:
+ * I. Create a function that returns the number of digits;
+ * II. Create a fuction that applies the formation rule of decimal numbers using the number
+ * of digits of the step I;
+ * III. With all of that at hand, create a function that loop through the array of
+ * step II verifying in which range each number of this array fall;
+ * IV. In the end, just return the number converted.
  * @brief Description of the problem: https://leetcode.com/problems/integer-to-roman/
  */
 
@@ -37,8 +41,10 @@ int numberOfDigits(int num){
 }
 
 /**
- * @brief This function will apply 
- * <a href:https://en.wikipedia.org/wiki/Positional_notation> notational position</a>
+ * @brief This function will expand the @param number through Notational Position. 
+ * For example, say number = 420, then the expanded form of this number will be
+ * 4*10² + 2*10¹ + 0*10^0. It's worth mention that as the function return an array of
+ * integers, that means the digits, the @param numDigits will be size of such array. 
  * @param number 
  * @param numDigits 
  * @return int* 
@@ -56,7 +62,12 @@ int* formationRuleDecimalNumbers(int number, int numDigits){
 }
 
 /**
- * @brief 
+ * @brief this function will use the @param decimalDigitsArray, that is the array
+ * that contains the expanded form of the decimal number, as a the guide to form the
+ * roman number. For example, taking the number 420 -> decimalDigitsArray[400][20][0].
+ * So, decimalDigitsArray[0] = 400 the thing is ask in which range of number 400 will
+ * fall and that's it. The same step will happen to the next numbers presented in the
+ * array.  
  * @param decimalDigitsArray 
  * @param numDigts 
  * @return char* 
@@ -67,7 +78,8 @@ char* convertIntToRoman(int* decimalDigitsArray, int numDigts){
             if(decimalDigitsArray[i] == 4){
                 romanNumbersArray[j] = 'I';
                 romanNumbersArray[j + 1] = 'V';
-                romanNumbersArray[j + 2] = '\0';
+                romanNumbersArray[j + 2] = '\0';//this '\0' is here cuz is a string
+                //literal that will receive the roman number in the end
                 j = j + 2;
             }else{
                 for(int k = 0; k < decimalDigitsArray[i]; k++, j++){
@@ -157,7 +169,9 @@ char* convertIntToRoman(int* decimalDigitsArray, int numDigts){
 }
 
 /**
- * @brief this function convert int to roman number
+ * @brief this function executes other auxiliary functions that will help to get the
+ * the integer number converted to a roman number. In the end it just returns the ro-
+ * man number 
  * @param number 
  * @return char* 
  */
@@ -175,7 +189,7 @@ char* intToRoman(int number){
 }
 
 int main(){
-    char* result = NULL; 
+    char* result = NULL;//string literal
     for(int i = 1; i <= 1000; i++){
         result = intToRoman(i);
         printf("Output: %s\n", result);
